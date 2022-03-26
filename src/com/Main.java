8302c4be30d3baf01b7059bcaf6855a2ccde1f29
RomanV521 <romav5166@gmail.com>
2022-03-26 21:28:41 +0200

    Complete

--- src/com/Main.java
+++ src/com/Main.java
@@ -13,37 +13,49 @@ public class Main {
         int t1, k, t2;
         int n;
         int[] a = new int[h];
-        System.out.println(h);
+        System.out.println("\tРеализовать функцию перемешивания массива целых чисел (кол-во элементов > 100) блоками по K элементов N раз.\n\n"+"Количество элементов: "+h+"\n\nИсходный массив:");
 
         for(int i=0; i<h; i++){
             a[i]=random.nextInt(10)+1; //random number(1..10)
             System.out.print(" "+a[i]);
         }
 
-        System.out.print("\nВведите количество повторов N: ");
+        System.out.print("\n\nВведите количество повторов N: ");
         n=scanner.nextInt();
-        System.out.print("\nВведите количество элементов K: ");
+        System.out.print("Введите количество элементов в блоке K: ");
         k=scanner.nextInt();
 
+        System.out.println("\nРезультат:");
         while (n>0){
             t1 = random.nextInt(h-k);//задаем начальную точку
             t2 = k+t1;//конечная точка
             //System.out.print(" "+t1);
-            if(t2>=h-5){
+            if(t2>=h-35){
                 while (t1<=t2){
-                    a[t1]=a[t1-3];
+                    a[t1]=a[t1-5];
                     t1++;
                 }
             }
             else {
-                if (t2<=5){
+                if (t2<=35){
                     while (t1<=t2){
                         a[t1]=a[t1+4];
                         t1++;
                     }
                 }
+                else{
+                    while (t1<=t2){
+                        a[t1]=a[t1+15];
+                        t1++;
+                    }
+                }
             }
             n--;
+
+            /**
+             *  | проверка
+             * \/
+             */
             System.out.println(" ");
             for(int i=0; i<h; i++){
                 System.out.print(" "+a[i]);
